021c76027f31312c311a51c207f5037b4895da98
Melih Burak Memiş <melihburakmemis@Melih-MacBook-Air.local>
2025-07-15 01:13:48 +0300

    Fix token security

--- version.py
+++ version.py
@@ -2,7 +2,7 @@
 Version information for clapp-pm package.
 """
 
-__version__ = "1.0.15"
+__version__ = "1.0.16"
 __author__ = "Melih Burak Memiş"
 __email__ = "mburakmemiscy@gmail.com"
 __description__ = "Lightweight cross-language app manager for Python and Lua" 
