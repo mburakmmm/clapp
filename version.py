eb5097272246b7d40d98f16d2731dc727cbe1a1b
Melih Burak Memiş <melihburakmemis@Melih-MacBook-Air.local>
2025-07-15 01:01:06 +0300

    Update version to 1.0.11

--- version.py
+++ version.py
@@ -2,7 +2,7 @@
 Version information for clapp-pm package.
 """
 
-__version__ = "1.0.10"
+__version__ = "1.0.11"
 __author__ = "Melih Burak Memiş"
 __email__ = "mburakmemiscy@gmail.com"
 __description__ = "Lightweight cross-language app manager for Python and Lua" 
