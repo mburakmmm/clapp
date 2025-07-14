691033f105c95801dc84b63a5ba9e0cbc8c7bd25
Melih Burak Memiş <melihburakmemis@Melih-MacBook-Air.local>
2025-07-15 01:05:41 +0300

    Add secure token management and deployment script - v1.0.12

--- clapp_pm-1.0.11/version.py
+++
@@ -1,8 +0,0 @@
-"""
-Version information for clapp-pm package.
-"""
-
-__version__ = "1.0.11"
-__author__ = "Melih Burak Memiş"
-__email__ = "mburakmemiscy@gmail.com"
-__description__ = "Lightweight cross-language app manager for Python and Lua" 
