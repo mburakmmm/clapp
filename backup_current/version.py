388c0a254a623d0367f4f30b3a967246b04c09d2
Melih Burak Memiş <mburakmemiscy@gmail.com>
2025-07-29 17:33:07 +0300

    🗑️ Remove development and test directories from repository

--- backup_current/version.py
+++
@@ -1,8 +0,0 @@
-"""
-Version information for clapp-pm package.
-"""
-
-__version__ = "1.0.1"
-__author__ = "Melih Burak Memiş"
-__email__ = "mburakmemiscy@gmail.com"
-__description__ = "Lightweight cross-language app manager for Python and Lua" 
